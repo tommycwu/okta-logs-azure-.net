3d962da43a59a365d92b598c829236f4b59aa2cd
tommycwu <tommycwu@yahoo.com>
2020-04-28 20:09:47 -0700

    Initial commit

---
+++ FunctionInsights.cs
@@ -0,0 +1,103 @@
+using System;
+using System.IO;
+using System.Net;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace FunctionAppChevronPOC
+{
+    public  class FunctionInsights
+    {
+        string instrumentationKey = "0bf129fc-c9c1-4d72-b200-c21fe882828f";
+        string logApiKey = "013o4tpCxcfY9vGHmp4VRyHbWX-b9ebbBFSntE-oq4";
+        string logApiUrl = "https://yourorg.oktapreview.com/api/v1/logs";
+
+
+        private string GetLogs(string logUrl, string apiKey)
+        {
+            string responseText = "";
+            try
+            {
+                var webRequest = WebRequest.Create(logUrl) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    WebResponse response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+                        JToken parsedJson = JToken.Parse(jsonObject.ToString());
+                        responseText = parsedJson.ToString(Formatting.None);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return responseText;
+        }
+
+        [FunctionName("FunctionInsights")]
+        public void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"Insights function executed at: {DateTime.Now}");
+            try
+            {
+                DateTime nowMinusOne = DateTime.UtcNow.AddMinutes(-1);
+                string fullLogApiUrl = logApiUrl + "?since=" + nowMinusOne.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+                    + "&sortOrder=DESCENDING";
+
+                string jsonString = GetLogs(fullLogApiUrl, logApiKey);
+
+                if (jsonString + "" != "")
+                {
+                    string filteredString = "[" + jsonString.TrimEnd(jsonString[jsonString.Length - 1]) + "]";
+                    dynamic jsonObject = JsonConvert.DeserializeObject(filteredString);
+                    int i = 0;
+                    foreach (dynamic item in jsonObject)
+                    {
+                        string tempStr = item.ToString();
+                        string eventStr = "";
+                        if (tempStr.Contains("FAIL"))
+                        {
+                            eventStr = item.displayMessage;
+                            var config = new TelemetryConfiguration();
+                            config.InstrumentationKey = instrumentationKey;
+                            TelemetryClient client = new TelemetryClient(config);
+                            client.Context.User.Id = item.actor.alternateId;
+                            client.Context.User.AccountId = item.actor.displayName;
+                            client.Context.User.AuthenticatedUserId = item.client.ipAddress;
+                            client.Context.User.UserAgent = item.client.userAgent.rawUserAgent;
+                            client.Context.Session.Id = item.client.geographicalContext.country + ", " + item.client.geographicalContext.State + ", " +
+                                item.client.geographicalContext.city + ", " + item.client.geographicalContext.postalCode;
+                            client.TrackEvent(eventStr);
+                            log.LogInformation("Event sent at " + DateTime.Now.ToLocalTime().ToString());
+                        }
+                        else
+                        {
+                            i++;
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    log.LogInformation("Nothing sent at " + DateTime.Now.ToLocalTime().ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+            }
+        }
+    }
+}
---
+++ FunctionSentinel.cs
@@ -0,0 +1,140 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppChevronPOC
+{
+    public class FunctionSentinel
+    {
+        string logApiKey = "013o4tpCxcfY9vGHmp4VRyHbWX-b9ebbBFSntE-oq4";
+        string logApiUrl = "https://yourorg.oktapreview.com/api/v1/logs";
+        string workspaceId = "af9ddddd-c440-48fd-b1e4-25b6d2be9419";
+        string pk = "8auE/3CAFw/K5BY+/wZn14bXEiueWmd01pV9O10zWmNkur2mwPXBGvatXKBq1Y4ctqnBSqFneADet1e6qXhSqA==";
+        string sentinelLogName = "Test";
+
+        private static string BuildSignature(string message, string secret)
+        {
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = Convert.FromBase64String(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hash = hmacsha256.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private string PostData(string postUrl, string logType, string authSignature, string xmsDate, string json)
+        {
+            try
+            {
+                var client = new System.Net.Http.HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("Log-Type", logType);
+                client.DefaultRequestHeaders.Add("Authorization", authSignature);
+                client.DefaultRequestHeaders.Add("x-ms-date", xmsDate);
+                client.DefaultRequestHeaders.Add("time-generated-field", "");
+
+                var httpContent = new StringContent(json, Encoding.UTF8);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                Task<System.Net.Http.HttpResponseMessage> response = client.PostAsync(postUrl, httpContent);
+
+                System.Net.Http.HttpContent responseContent = response.Result.Content;
+                string result = responseContent.ReadAsStringAsync().Result + "";
+                if (result == "")
+                {
+                    return "Return Result: Success - " + DateTime.Now.ToLocalTime().ToString(); ;
+                }
+                else
+                {
+                    return "Return Result: " + result;
+                };
+            }
+            catch (Exception ex)
+            {
+                return "API Post Exception: " + ex.Message;
+            }
+        }
+
+        private string GetLogs(string logUrl, string apiKey)
+        {
+            string responseText = "";
+            try
+            {
+                var webRequest = WebRequest.Create(logUrl) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    WebResponse response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+                        JToken parsedJson = JToken.Parse(jsonObject.ToString());
+                        responseText = parsedJson.ToString(Formatting.None);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return responseText;
+        }
+
+        [FunctionName("FunctionSentinel")]
+        public void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation("Polling began at: " + DateTime.Now.ToString());
+            try
+            {
+                DateTime nowMinusOne = DateTime.UtcNow.AddMinutes(-1);
+                string fullLogApiUrl = logApiUrl + "?since=" + nowMinusOne.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+                    + "&sortOrder=DESCENDING";
+                
+                string jsonString = GetLogs(fullLogApiUrl, logApiKey);
+
+
+                if (jsonString + "" != "")
+                {
+                    dynamic jsonObject = JsonConvert.DeserializeObject("[" + jsonString.TrimEnd(jsonString[jsonString.Length - 1]) + "]");
+
+                    string dateString = DateTime.UtcNow.ToString("r");
+                    string sentinelUrl = "https://" + workspaceId + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";
+
+                    foreach (dynamic jsonItem in jsonObject)
+                    {
+                        byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonItem.ToString());
+                        string stringToHash = "POST\n" + jsonBytes.Length + "\napplication/json\n" + "x-ms-date:" + dateString + "\n/api/logs";
+                        string hashedString = BuildSignature(stringToHash, pk);
+                        string signature = "SharedKey " + workspaceId + ":" + hashedString;
+                        string itemString = jsonItem.ToString();
+                        string resultString = PostData(sentinelUrl, sentinelLogName, signature, dateString, itemString);
+                        log.LogInformation(resultString);
+                    }
+                }
+                else
+                {
+                    log.LogInformation("Nothing sent at " + DateTime.Now.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+            }
+
+        }
+    }
+}
